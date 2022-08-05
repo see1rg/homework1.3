941688954f148fc95bb66052f4cb773847766725
see1rg <see1rg@mail.ru>
2022-08-05 14:41:46 +0300

    сделал дз 6

--- src/homework.java
+++ src/homework.java
@@ -1,61 +1,65 @@
 public class homework {
 
     public static void main(String[] args) {
-    task1();
-    task2();
-    task3();
-    task4();
-    task5();}
-    public static void task1(){
+        task1();
+        task2();
+        task3();
+        task4();
+        task5();
+        task6();
+    }
+
+    public static void task1() {
         byte clientOS = 1;
         if (clientOS == 1) {
-            System.out.println("Установите версию приложения для Android по ссылке.");}
-        else if (clientOS == 0) {
+            System.out.println("Установите версию приложения для Android по ссылке.");
+        } else if (clientOS == 0) {
             System.out.println("Установите версию приложения для iOS по ссылке.");
         }
         System.out.println("операционная система не распознана.");
     }
 
-    public static void task2(){
+    public static void task2() {
         byte clientOS = 1;
         int clientDeviceYear = 2014;
+
         if (clientOS == 1 && clientDeviceYear < 2015) {
             System.out.println("Установите облегченную версию приложения для Android по ссылке.");
-        }
-        else if (clientOS == 1 && clientDeviceYear >= 2015) {
+        } else if (clientOS == 1 && clientDeviceYear >= 2015) {
             System.out.println("Установите версию приложения для Android по ссылке.");
-        }
-        else if (clientOS == 0 && clientDeviceYear < 2015) {
+        } else if (clientOS == 0 && clientDeviceYear < 2015) {
             System.out.println("Установите облегченную версию приложения для IOs по ссылке.");
-        }
-        else if (clientOS == 0 && clientDeviceYear >= 2015) {
+        } else if (clientOS == 0 && clientDeviceYear >= 2015) {
             System.out.println("Установите версию приложения для IOs по ссылке.");
-        }
-        else System.out.println(" Устройство не определено.");
+        } else System.out.println(" Устройство не определено.");
 
-        }
-        public static void task3(){
+    }
+
+    public static void task3() {
         int year = 2021;
-        if ((year %4 == 0 && year %100 != 0) || year %400 == 0 ) {
+        if ((year % 4 == 0 && year % 100 != 0) || year % 400 == 0) {
             System.out.println(year + " год является високосным. ");
         } else {
-            System.out.println( year + " год не является високосным. ");
+            System.out.println(year + " год не является високосным. ");
         }
     }
 
-    public static void task4(){
+    public static void task4() {
         int deliveryDistance = 95;
         int deliveryDays = 1;
 
         if (deliveryDistance > 20) {
-            deliveryDays++; }
+            deliveryDays++;
+        }
 
         if (deliveryDistance > 60) {
-            deliveryDays++;}
+            deliveryDays++;
+        }
 
-        System.out.println( "Потребуется дней : " + deliveryDays);
+        System.out.println("Потребуется дней : " + deliveryDays);
 
     }
+
     public static void task5() {
 
         int monthNumber = 2;
@@ -84,6 +88,34 @@ public class homework {
                 System.out.println("Номер месяца больше 12 или меньше 0");
         }
     }
+
+
+    public static void task6() {
+        int age = 19;
+        int salary = 58_000;
+        double limit = 0;
+        double koef1 = 1.5;
+        double koef2 = 1.2;
+
+        if (age >= 23) {
+            salary *= 3;
+            if (salary > 80_000) {
+                limit = salary * koef1;
+            } else if (salary < 80_000 && salary > 50_000) {
+                limit = salary * koef2;
+            }
+        }
+
+        if (age < 23) {
+            salary *= 2;
+            if (salary > 80_000) {
+                limit = salary * koef1;
+            } else if (salary < 80_000 && salary > 50_000) {
+                limit = salary * koef2;
+            }
+        }
+        System.out.println("Мы готовы выдать вам кредитную карту с лимитом " + limit + " рублей");}
 }
+
 
 
